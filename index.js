4538d2d878cda498a8cd5653ec837b88b8e8cd23
razdvapoka <razdvapoka@gmail.com>
2020-05-02 15:48:23 +0300

    :sparkles: Add websockets

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', socket => {
+  socket.on('chat message', msg => {
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, () => {
