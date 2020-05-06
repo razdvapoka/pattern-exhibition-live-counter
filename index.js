beb4b7ee3c7aa38862c8d9d6bcae69f30f1cdc49
razdvapoka <razdvapoka@gmail.com>
2020-05-06 19:25:35 +0300

    :sparkles: Replace chat with user count

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+let userCount = 0;
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', socket => {
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg);
+  let isUserAdded = false;
+  socket.on('add user', () => {
+    if (!isUserAdded) {
+      isUserAdded = true;
+      userCount++;
+      io.sockets.emit('user joined', {
+        userCount,
+      });
+    }
+  });
+  socket.on('disconnect', () => {
+    if (isUserAdded) {
+      userCount--;
+      io.sockets.emit('user left', {
+        userCount,
+      });
+    }
   });
 });
 
